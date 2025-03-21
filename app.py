8904bbcf072b26030866a5443aa83e5391ac4de2
vinicincosta <viniciusgcosta2603@gmail.com>
2025-03-21 16:15:52 -0300

    21-03

--- app.py
+++ app.py
@@ -1,40 +1,48 @@
-# importar biblioteca
-from flask import Flask, jsonify, render_template
-# importe para documentacao
-from flask_pydantic_spec import FlaskPydanticSpec
+# Importar bibliotecas
 import datetime
+
+from flask import Flask, jsonify
+from flask_pydantic_spec import FlaskPydanticSpec
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
-# [flask routes] para listar rotas da api
-
-# criar variavel para receber a classe Flask
+# Criar a variável para receber a classe Flask
 app = Flask(__name__)
-
-#   documentacao OpenAPI
+tempo = datetime.now()
+# Documentação OpenAPI
 spec = FlaskPydanticSpec('flask',
                          title='First API - SENAI',
                          version='1.0.0')
 spec.register(app)
 
-@app.route('/validadealunos')
-def validado():
-    prazo = 12
-    meses = datetime.today()+relativedelta(months=prazo)
-    # years=
-    anos = ''
-    # weeks=
-    semanas = ''
-    # days=
-    dias = ''
 
-    return (f'"antes" - {datetime.today().strftime("%d-%m-%Y")}, '
-            f'"dias"- {dias}, '
-            f'"semanas"- {semanas}, '
-            f'"meses"- {meses},'
-            f'"anos"- {anos}')
+@app.route('/validadealunos/ano/mes/dia')
+def validade_alunos(dia,mes,ano):
+    dia_ = int(dia)
+    mes_ = int(mes)
+    ano_ = int(ano)
+    data_informada = datetime.datetime(ano,mes, dia)
+
+    prazo_em_meses = 12  # Prazo de validade em meses
+    hoje = datetime.today()  # Data de hoje
+    validade = hoje + relativedelta(months=prazo_em_meses)  # Data futura após o prazo
+
+    # Calculos
+    delta = validade - hoje
+    dias = delta.days  # Total de dias
+    semanas = dias // 7  # Total de semanas
+    anos = delta.days // 365  # Aproximação do número de anos
 
 
-# iniciar servidor
+    return jsonify ({
+        "data_atual": hoje.strftime("%d-%m-%Y"),
+        "data_validade": validade.strftime("%d-%m-%Y"),
+        "dias": dias,
+        "semanas": semanas,
+        "meses": prazo_em_meses,
+        "anos": anos
+    })
+
+
 if __name__ == '__main__':
     app.run(debug=True)
